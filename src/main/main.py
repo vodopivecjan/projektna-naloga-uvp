88bc4187fe80988d8dbbc1342a62a2bb2065d4dc
vodopivecjan <jv45293@student.uni-lj.si>
2025-07-04 20:51:29 +0200

    created some scraping using beautifulsoup, realized that its limited to 25 results, so switching to playwright

--- src/main/main.py
+++ src/main/main.py
@@ -1,2 +1,23 @@
+# import os
+import csv
+
+
+from main.vars import FIELDNAMES
+from main.scraping.scrape import scrape_imdb_top_tv
+
+
 def main():
-    pass
+    top_tv = scrape_imdb_top_tv()
+
+    # print(os.getcwd())
+
+    # Save to CSV
+    with open("../output/top_tv_shows.csv", "w", newline="", encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames=FIELDNAMES)
+        writer.writeheader()
+        writer.writerows(top_tv)
+
+    # for show in top_tv[:5]:
+    #     print(
+    #         f"{show['rank']}. {show['title']} ({show['year']}) - {show['rating']} - {show['url']}"
+    #     )
